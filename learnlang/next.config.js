5f6db4dea7da9856494b01621331026d03b1d013
ck18sss <ck18sss@mail.com>
2023-12-27 14:28:37 -0500

    cleaning

--- learnlang/next.config.js
+++ learnlang/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-    images: {
-    
-    },
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
   
     async redirects() {
       return [
