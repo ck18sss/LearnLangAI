12162e0999bde7da6c1449b17ed744c0598c83c4
ck18sss <ck18sss@mail.com>
2023-12-28 14:04:53 -0500

    cleaning
    cleaning

--- learnlang/src/app/auth-callback/page.tsx
+++ learnlang/src/app/auth-callback/page.tsx
@@ -1,38 +1,42 @@
-"use client"
+'use client'
 
+import { Loader2 } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { trpc } from '../_trpc/client'
-import { Loader2 } from 'lucide-react'
 
 const Page = () => {
-  const router = useRouter()
+  const route = useRouter()
 
   const searchParams = useSearchParams()
+
   const origin = searchParams.get('origin')
+
+  console.log('auth callback page')
 
   trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
+      console.log(success, 'success in auth callback page')
+
       if (success) {
-        // user is synced to db
-        router.push(origin ? `/${origin}` : '/dashboard')
+        route.push(origin ? `/${origin}` : '/dashboard')
       }
     },
     onError: (err) => {
-      if (err.data?.code === 'UNAUTHORIZED') {
-        router.push('/sign-in')
+      console.log(err, 'error in auth callback page')
+
+      if (err.data?.code == 'UNAUTHORIZED') {
+        route.push('/sign-in')
       }
     },
-    retry: true,
+    retry: false,
     retryDelay: 500,
   })
 
   return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Setting up your account...
-        </h3>
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
         <p>You will be redirected automatically.</p>
       </div>
     </div>
